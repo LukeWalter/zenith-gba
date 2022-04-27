01cf81b2de37f7a2b5d8bb6e7a7d97a77a6939a0
Luke Walter <lwalter3@gatech.edu>
2022-04-27 14:29:38 -0400

    Added final title screen

--- zenithtitle.h
+++ zenithtitle.h
@@ -1,25 +1,25 @@
 
-//{{BLOCK(donkeykongtitle)
+//{{BLOCK(zenithtitle)
 
 //======================================================================
 //
-//	donkeykongtitle, 256x256@4, 
+//	zenithtitle, 256x256@4, 
 //	+ palette 256 entries, not compressed
-//	+ 1024 tiles not compressed
+//	+ 558 tiles (t|f|p reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 32x32 
-//	Total size: 512 + 32768 + 2048 = 35328
+//	Total size: 512 + 17856 + 2048 = 20416
 //
-//	Time-stamp: 2022-03-19, 19:26:28
+//	Time-stamp: 2022-04-27, 14:26:49
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
 //======================================================================
 
-#ifndef GRIT_DONKEYKONGTITLE_H
-#define GRIT_DONKEYKONGTITLE_H
+#ifndef GRIT_ZENITHTITLE_H
+#define GRIT_ZENITHTITLE_H
 
-#define zenithtitleTilesLen 32768
-extern const unsigned short zenithtitleTiles[16384];
+#define zenithtitleTilesLen 17856
+extern const unsigned short zenithtitleTiles[8928];
 
 #define zenithtitleMapLen 2048
 extern const unsigned short zenithtitleMap[1024];
@@ -27,6 +27,6 @@ extern const unsigned short zenithtitleMap[1024];
 #define zenithtitlePalLen 512
 extern const unsigned short zenithtitlePal[256];
 
-#endif // GRIT_DONKEYKONGTITLE_H
+#endif // GRIT_ZENITHTITLE_H
 
-//}}BLOCK(donkeykongtitle)
+//}}BLOCK(zenithtitle)
