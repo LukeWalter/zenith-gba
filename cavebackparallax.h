a6647cb367091cf75da6036c308b84d0f285903f
Luke Walter <59621677+LukeWalter@users.noreply.github.com>
2022-04-29 08:30:22 -0400

    Removed black space between front parallax bkgd cycles

--- cavebackparallax.h
+++ cavebackparallax.h
@@ -5,11 +5,11 @@
 //
 //	cavebackparallax, 256x256@4, 
 //	+ palette 256 entries, not compressed
-//	+ 327 tiles (t|f|p reduced) not compressed
+//	+ 345 tiles (t|f|p reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 32x32 
-//	Total size: 512 + 10464 + 2048 = 13024
+//	Total size: 512 + 11040 + 2048 = 13600
 //
-//	Time-stamp: 2022-04-29, 07:31:56
+//	Time-stamp: 2022-04-29, 08:26:25
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_CAVEBACKPARALLAX_H
 #define GRIT_CAVEBACKPARALLAX_H
 
-#define cavebackparallaxTilesLen 10464
-extern const unsigned short cavebackparallaxTiles[5232];
+#define cavebackparallaxTilesLen 11040
+extern const unsigned short cavebackparallaxTiles[5520];
 
 #define cavebackparallaxMapLen 2048
 extern const unsigned short cavebackparallaxMap[1024];
